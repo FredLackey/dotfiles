b1ebff52d49cd950f65502d3c3d729af94b96f97
Fred Lackey <fred.lackey@gmail.com>
2021-07-09 17:51:02 -0400

    baseline

---
+++ src/os/create_directories.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+create_directories() {
+
+    declare -a DIRECTORIES=(
+        "$HOME/Downloads/_Source"
+        "$HOME/Downloads/_YouTube"
+        "$HOME/Source"
+        "$HOME/Screenshots"
+        "$HOME/Temporary"
+    )
+
+    for i in "${DIRECTORIES[@]}"; do
+        mkd "$i"
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    print_in_purple "\n • Create directories\n\n"
+    create_directories
+}
+
+main
